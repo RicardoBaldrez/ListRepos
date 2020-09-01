8565b0f244687850671bd54b14f57b177403ddd7
Ricardo <ricardo.baldrez@gmail.com>
2020-09-01 01:38:24 -0300

    Renderizando em tela

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ class App {
         this.repositories = [];
 
         this.formElement = document.getElementById('repo-form');
+        this.listElement = document.getElementById('repo-list');
 
         this.registerHandlers();
     }
@@ -23,7 +24,36 @@ class App {
             }
         );
 
-        console.log(this.repositories);
+        this.render();
+    }
+
+    render() {
+        this.listElement.innerHTML = '';
+
+        this.repositories.forEach(repo => {
+            let imgElement = document.createElement('img');
+            imgElement.setAttribute('src', repo.avatar_url);
+
+            let titleElement = document.createElement('strong');
+            titleElement.appendChild(document.createTextNode(repo.name));
+
+            let descElement = document.createElement('p');
+            descElement.appendChild(document.createTextNode(repo.description));
+
+            let linkElement = document.createElement('a');
+            linkElement.setAttribute('href', repo.html_url);
+            linkElement.setAttribute('_target', '_blank');
+            linkElement.appendChild(document.createTextNode('Acessar'));
+
+            let listItemElement = document.createElement('li');
+            listItemElement.appendChild(imgElement);
+            listItemElement.appendChild(titleElement);
+            listItemElement.appendChild(descElement);
+            listItemElement.appendChild(linkElement);
+
+
+            this.listElement.appendChild(listItemElement);
+        });
     }
 }
 
