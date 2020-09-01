10a60a961b52f500da1cb7ca52c2313530ad02cf
Ricardo <ricardo.baldrez@gmail.com>
2020-09-01 01:16:32 -0300

    Adicionar repositórios - Criação da classe

---
+++ src/main.js
@@ -0,0 +1,30 @@
+class App {
+    constructor() {
+        this.repositories = [];
+
+        this.formElement = document.getElementById('repo-form');
+
+        this.registerHandlers();
+    }
+
+    registerHandlers() {
+        this.formElement.onsubmit = event => this.addRepository(event);
+    }
+
+    addRepository(event) {
+        event.preventDefault();
+
+        this.repositories.push(
+            {
+                name: 'rocketseat.com.br',
+                description: 'Tire a sua ideia do papel, e de vida a sua startup.',
+                avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
+                html_url: 'http://github.com/rocketseat/rocketseat.com.br'
+            }
+        );
+
+        console.log(this.repositories);
+    }
+}
+
+new App();
